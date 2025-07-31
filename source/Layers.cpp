b7705182e1444300e93239bf426e81a0623ca719
Mann Makwana <aiwithmann@gmail.com>
2025-07-31 18:05:31 +0530

    Update Layers.cpp

--- source/Layers.cpp
+++ source/Layers.cpp
@@ -7,70 +7,36 @@
 static std::random_device rd;
 static std::mt19937 gen(rd());
 
-Linear::Linear(int numInputs, int numOutputs, ActivationFunction activationFn, bool initialize)
-    : numInputs(numInputs), numOutputs(numOutputs), activation(activationFn), dropoutRate(0.1f) {
+void Layer::setTraining(bool mode){
+    isTraining = mode;
+}
 
-    weights = Eigen::MatrixXf::Zero(numOutputs, numInputs);
-    biases = Eigen::VectorXf::Zero(numOutputs);
+Linear::Linear(int nInputs, int nOutputs, float low, float high){
+    this->B = Eigen::MatrixXf::Zero(nOutputs,1);
+    this->bGrad.resize(nOutputs,1);
+    this->W.resize(nOutputs,nInputs);
+    this->wGrad.resize(nOutputs, nInputs);
+    std::uniform_real_distribution<float> dist(low,high);
+    W = W.unaryExpr([&](float){return dist(gen);});
+}
 
-    if (initialize) {
-        std::normal_distribution<float> dist(0.0f, 1.0f / std::sqrt(numInputs));
-        for (int i = 0; i < weights.rows(); ++i)
-            for (int j = 0; j < weights.cols(); ++j)
-                weights(i, j) = dist(gen);
+Eigen::MatrixXf Linear::forward(const Eigen::MatrixXf& input){
+    if (input.size()!=B.size()){
+        throw std::invalid_argument("Shape mismatch");
     }
-
-    output = Eigen::VectorXf::Zero(numOutputs);
+    this->inputCache = input;
+    Eigen::MatrixXf Z = W * input + B;
+    return Z;
+}
+Eigen::MatrixXf Linear::backward(const Eigen::MatrixXf& gradOutputs){
+    
 }
 
-void Linear::forward(const Eigen::VectorXf& inputVec, bool isTraining) {
-    if (inputVec.size() != numInputs)
-        throw std::invalid_argument("Input vector size does not match layer input size.");
-
-    input = inputVec;
-    output = (weights * input + biases);
-    applyActivation();
-
-    if (isTraining) {
-        applyDropout();
-    }
+std::vector<Eigen::MatrixXf*> Linear::getParameters(){
+    return {&W,&B};
 }
 
-const Eigen::VectorXf& Linear::getOutput() const {
-    return output;
+std::vector<Eigen::MatrixXf*> Linear::getGradients(){
+    return {&wGrad, &bGrad};
 }
 
-int Linear::getnumOutputs() const {
-    return numOutputs;
-}
-
-void Linear::applyActivation() {
-    switch(this->activation){
-        case ActivationFunction::RELU:
-            output = output.array().max(0.0f);
-            break;
-        case ActivationFunction::SIGMOID:
-            output = (1.0f / (1.0f + (-output.array()).exp())).matrix();
-            break;
-        case ActivationFunction::TANH:
-            output = output.array().tanh();
-            break;
-        default:
-            throw std::invalid_argument("Activation function is not from 'RELU', 'SIGMOID' or 'TANH'");
-
-    }
-}
-
-void Linear::applyDropout() {
-    if (dropoutRate <= 0.0f || dropoutRate >= 1.0f)
-        return;
-
-    std::bernoulli_distribution keepProb(1.0f - dropoutRate);
-    for (int i = 0; i < output.size(); ++i) {
-        if (!keepProb(gen)) {
-            output[i] = 0.0f;
-        } else {
-            output[i] /= (1.0f - dropoutRate);  // Inverted dropout
-        }
-    }
-}
