63b2a04034a660427de420c76a5aa2bd767c49ae
Mann Makwana <aiwithmann@gmail.com>
2025-08-01 08:55:27 +0530

    Make Linear compatible for batched inputs and outputs

--- source/Layers.cpp
+++ source/Layers.cpp
@@ -11,21 +11,25 @@ void Layer::setTraining(bool mode){
     isTraining = mode;
 }
 
-Linear::Linear(int nInputs, int nOutputs, float low, float high){
+Linear::Linear(int nInputs, int nOutputs, float low, float high, float dropout){
     this->B = Eigen::MatrixXf::Zero(nOutputs,1);
     this->bGrad.resize(nOutputs,1);
     this->W.resize(nOutputs,nInputs);
     this->wGrad.resize(nOutputs, nInputs);
+    this->dropout = dropout;
+
     std::uniform_real_distribution<float> dist(low,high);
     W = W.unaryExpr([&](float){return dist(gen);});
 }
 
 Eigen::MatrixXf Linear::forward(const Eigen::MatrixXf& input){
-    if (input.size()!=B.size()){
+    //Input must be nInputs * BatchSize
+    if (input.rows()!=W.cols()){
         throw std::invalid_argument("Shape mismatch");
     }
     this->inputCache = input;
-    Eigen::MatrixXf Z = W * input + B;
+    Eigen::MatrixXf Z = (W * input).colwise() + B.col(0);
+    if(isTraining) { applyDropout(Z); }
     return Z;
 }
 Eigen::MatrixXf Linear::backward(const Eigen::MatrixXf& gradOutputs){
@@ -40,3 +44,10 @@ std::vector<Eigen::MatrixXf*> Linear::getGradients(){
     return {&wGrad, &bGrad};
 }
 
+void Linear::applyDropout(Eigen::MatrixXf& Z){
+    std::bernoulli_distribution dist(1.0f - this->dropout);
+    auto bernouli = [&](){return dist(gen);};
+    Eigen::MatrixXf DP = Eigen::MatrixXf::NullaryExpr(Z.rows(),Z.cols(), bernouli).cast<float>();
+    this->dropoutMask = DP;
+    Z = Z.cwiseProduct(DP);
+}
