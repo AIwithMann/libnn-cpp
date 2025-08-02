d803fed2457ba0ca2082484a85bad0c060d0ba84
Mann Makwana <aiwithmann@gmail.com>
2025-08-02 10:28:10 +0530

    Imporve Layer.h and adding new Layers

--- headers/Layers.h
+++ headers/Layers.h
@@ -2,40 +2,59 @@
 
 #include <Eigen/Dense>
 #include <vector>
-#include <string>
-
-enum class ActivationFunction {
-    RELU,
-    SIGMOID,
-    TANH
-};
 
 class Layer {
 protected:
     bool isTraining = true;
-    Eigen::MatrixXf inputCache; //Batchsize * nInputs
-    Eigen::MatrixXf output; //Batchsize * nOutputs
-    
+    Eigen::MatrixXf inputCache;
+    Eigen::MatrixXf output;
+
 public:
     virtual ~Layer() = default;
+
     virtual void setTraining(bool mode);
-    virtual Eigen::MatrixXf forward(const Eigen::MatrixXf& input);
-    virtual Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput);
-    virtual void update(float learning_rate);
-    virtual std::vector<Eigen::MatrixXf*> getParameters();
-    virtual std::vector<Eigen::MatrixXf*> getGradients();
+    virtual Eigen::MatrixXf forward(const Eigen::MatrixXf& input) = 0;
+    virtual Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) = 0;
+    virtual void update(float learningRate) {}
+    virtual std::vector<Eigen::MatrixXf*> getParameters() { return {}; }
+    virtual std::vector<Eigen::MatrixXf*> getGradients() { return {}; }
 };
 
-class Linear:public Layer{
+class Linear : public Layer {
 private:
-    Eigen::MatrixXf W, B, wGrad, bGrad,dropoutMask;
-    float dropout;
-    inline void applyDropout(Eigen::MatrixXf& Z);
+    Eigen::MatrixXf W, B;       
+    Eigen::MatrixXf wGrad, bGrad;
+    Eigen::MatrixXf dropoutMask;
+    float dropout = 0.0f;
+
+    void applyDropout(Eigen::MatrixXf& Z);
+
 public:
-    inline Linear(int nInputs, int nOutputs, float low, float high, float dropout);
-    inline ~Linear()=default;
-    inline Eigen::MatrixXf forward(const Eigen::MatrixXf& input);
-    inline Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutputs);
-    inline std::vector<Eigen::MatrixXf*> getParameters();
-    inline std::vector<Eigen::MatrixXf*> getGradients();
+    Linear(int nInputs, int nOutputs, float dropout = 0.0f);
+
+    Eigen::MatrixXf forward(const Eigen::MatrixXf& input) override;
+    Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutputs) override;
+    void update(float learningRate) override;
+
+    std::vector<Eigen::MatrixXf*> getParameters() override;
+    std::vector<Eigen::MatrixXf*> getGradients() override;
 };
+
+class ReLU : public Layer {
+public:
+    Eigen::MatrixXf forward(const Eigen::MatrixXf& input) override;
+    Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
+};
+
+class Sigmoid : public Layer {
+public:
+    Eigen::MatrixXf forward(const Eigen::MatrixXf& input) override;
+    Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
+};
+
+class Tanh : public Layer {
+public:
+    Eigen::MatrixXf forward(const Eigen::MatrixXf& input) override;
+    Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
+};
+
