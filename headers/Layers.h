69a24f63f90062432f44208bf567840599623880
Mann Makwana <aiwithmann@gmail.com>
2025-08-05 14:43:24 +0530

    Update Layers.h

--- headers/Layers.h
+++ headers/Layers.h
@@ -8,6 +8,7 @@ protected:
     bool isTraining = true;
     Eigen::MatrixXf inputCache;
     Eigen::MatrixXf output;
+    bool istrainable = true;
 
 public:
     virtual ~Layer() = default;
@@ -17,6 +18,7 @@ public:
     virtual Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) = 0;
     virtual void update(float learningRate) {}
     virtual std::vector<Eigen::MatrixXf*> getParameters() { return {}; }
+    virtual bool isTrainable() const { return isTrainable; }
     virtual std::vector<Eigen::MatrixXf*> getGradients() { return {}; }
 };
 
@@ -26,34 +28,43 @@ private:
     Eigen::MatrixXf wGrad, bGrad;
     Eigen::MatrixXf dropoutMask;
     float dropout = 0.0f;
+    bool istrainable = true;
 
     void applyDropout(Eigen::MatrixXf& Z);
 
 public:
     Linear(int nInputs, int nOutputs, float dropout = 0.0f);
-
+    bool isTrainable();
     Eigen::MatrixXf& forward(const Eigen::MatrixXf& input) override;
     Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutputs) override;
     void update(float learningRate) override;
-
+    bool isTrainable() const override{ return istrainable; }
     std::vector<Eigen::MatrixXf*> getParameters() override;
     std::vector<Eigen::MatrixXf*> getGradients() override;
 };
 
 class ReLU : public Layer {
+private:
+    bool istrainable = false;
 public:
+    bool isTrainable() const override{ return istrainable; }
     Eigen::MatrixXf& forward(const Eigen::MatrixXf& input) override;
     Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
 };
-
 class Sigmoid : public Layer {
+private:
+    bool istrainable = false;
 public:
+    bool isTrainable() const override{ return istrainable; }
     Eigen::MatrixXf& forward(const Eigen::MatrixXf& input) override;
     Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
 };
 
 class Tanh : public Layer {
+private:
+    bool istrainable = false;
 public:
+    bool isTrainable() const override{ return istrainable; }
     Eigen::MatrixXf& forward(const Eigen::MatrixXf& input) override;
     Eigen::MatrixXf backward(const Eigen::MatrixXf& gradOutput) override;
 };
