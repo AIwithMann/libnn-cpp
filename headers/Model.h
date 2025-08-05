0d92e7c9c79934b07d23720bc2a6a45ae95b743a
Mann Makwana <aiwithmann@gmail.com>
2025-08-05 14:43:46 +0530

    Update Model.h

--- headers/Model.h
+++ headers/Model.h
@@ -7,27 +7,30 @@
 #include "Dataset.h"
 #include<memory>
 #include<string>
+#include<any>
 class Model{
 private:
     int numLayers;
     int numInputs;
     int numOutputs;
     bool isTraining;
-    std::vector<std::unique_ptr<Layer>> Layers;
+    std::vector<std::shared_ptr<Layer>> Layers;
     Eigen::MatrixXf Output;
     float loss;
     Dataset& dataset;
     Loss lossType;
     void backward();
     void updateParams();
-    std::vector<Eigen::MatrixXf&> Gradients();
-
-    
 public:
     Model(int nInputs, int nOutputs, Dataset& ds);
     void setTraining(bool mode);
     void train(int epochs);
     Eigen::MatrixXf& forward(Eigen::MatrixXf& X);
     float calculateLoss(Eigen::MatrixXf& Ypred, Eigen::MatrixXf& Y);
-    void backward();
+    void backward(size_t batchIdx);
+    float lossGradient(Eigen::MatrixXf& Ypred, Eigen::MatrixXf& Y);
+    std::vector<std::shared_ptr<Layer>>& getLayers();
+    int getNumLayers();
+    int getNumInputs();
+    int getOutputs();
 };
