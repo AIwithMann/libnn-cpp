eed3c296b4a876750434eccb0b1e5e52310faa45
Mann Makwana <aiwithmann@gmail.com>
2025-08-08 08:07:06 +0530

    Update Optimizer.cpp

--- source/Optimizer.cpp
+++ source/Optimizer.cpp
@@ -68,3 +68,66 @@ void NAG::update(Batch& batch){
         i++;
     }
 }
+
+ADAGRAD::ADAGRAD(float lr, Trainables& trainables): lr(lr), trainables(trainables){}
+
+void ADAGRAD::init_state(){
+    for (auto& [param, grad]: trainables){
+        ParamState ps;
+        ps.G = Eigen::MatrixXf::Zero(param->rows(), param->cols());
+        state[param] = ps;
+    }
+}
+
+void ADAGRAD::update(){
+    for(auto& [param,grad] : trainables){
+        auto& ps = state[param];
+        ps.G += *grad * *grad;
+        *param -= lr * *grad / (ps.G.array().sqrt() + 1e-7);
+        grad->setZero();
+    }
+}   
+
+RMSPROP::RMSPROP(float lr, float dRate, Trainables& trainables): lr(lr), dRate(dRate), trainables(trainables){}
+
+void RMSPROP::init_state(){
+    for(auto& [param,grad]: trainables){
+        ParamState ps;
+        ps.G = Eigen::MatrixXf::Zero(param->rows(), param->cols());
+        state[param] = ps;
+    }
+}
+
+void RMSPROP::update(){
+    for(auto& [param, grad]: trainables){
+        auto& ps = state[param];
+        ps.G = dRate * ps.G.array() + (1 - dRate) * grad->array().square();
+        auto denom = (ps.G.array() + 1e-7).sqrt();
+        *param -= lr * grad->array() / denom;
+        grad->setZero();
+    }
+}
+
+ADAM::ADAM(float lr, float Beta1, float Beta2, Trainables& trainables):lr(lr), Beta1(Beta1), Beta2(Beta2), trainables(trainables){}
+
+void ADAM::init_state(){
+    for (auto& [param, grad]: trainables){
+        ParamState ps;
+        ps.m = Eigen::MatrixXf::Zero(param->rows(), param->cols());
+        ps.u = Eigen::MatrixXf::Zero(param->rows(), param->cols());
+        state[param] = ps;
+    }
+}
+
+void ADAM::update(){
+    for(auto& [param, grad]: trainables){
+        auto& ps = state[param];
+        ps.m = Beta1 * ps.m + (1 - Beta1) * grad->array();
+        ps.u = Beta2 * ps.u + (1 - Beta2) * grad->array().square();
+        ps.t++;
+        ps.m /= 1 - std::pow(Beta1, ps.t);
+        ps.u /= 1 - std::pow(Beta2, ps.t);
+        *param -= lr * ps.m / (ps.u.array().sqrt() + 1e-7);
+        grad->setZero();
+    }
+}
