a0b44bc78b2df20a43c10c2021acd078e9c7e3b4
Mann Makwana <aiwithmann@gmail.com>
2025-08-10 10:13:09 +0530

    Update Model.cpp

--- source/Model.cpp
+++ source/Model.cpp
@@ -1,6 +1,7 @@
 #include "Model.h"
 #include "Dataset.h"
-#include "Optimizer.h"
+#include "Loss.h"
+
 Model::Model(int nInputs, int nOutputs,Dataset& ds): numInputs(nInputs), numOutputs(nOutputs), dataset(ds){}
 
 void Model::setTraining(bool mode){ this->isTraining = mode; }
@@ -74,3 +75,40 @@ Trainables& Model::getTrainables(){
     }
     return trainables;
 }
+Eigen::MatrixXf Model::lossGradient(Eigen::MatrixXf& Ypred, Eigen::MatrixXf& Y){
+    switch(lossType){
+        case Loss::MSE:
+            return MSEgrad(Ypred, Y, 1);
+            break;
+        case Loss::MAE:
+            return MAEgrad(Ypred, Y, 1);
+            break;
+        case Loss::BINARY_CROSS_ENTROPY:
+            return BinaryCEgrad(Ypred, Y, 1);
+            break;
+        case Loss::CATEGORICAL_CROSS_ENTROPY:
+            return MultiCEgrad(Ypred, Y, 1);
+            break;
+        default:
+            throw std::invalid_argument("Loss type must be from MSE, MAE, BINARY_CROSS_ENTROPY or CATEGORICAL_CROSS_ENTROPY");
+    }
+}
+
+int Model::getNumLayers(){return numLayers;}
+int Model::getNumInputs(){return numInputs;}
+int Model::getNumOutputs(){return numOutputs;}
+
+void Model::updateParams(){
+    if (optim->state[0].t == 0){
+        optim->init_state(*this);
+        optim->state[0].t++;
+        optim->update();
+    }
+    else if(optim->state[0].t > 0){
+        optim->update();
+    }
+    else{
+        throw std::runtime_error("Optimizer state not initialized");
+    }
+}
+
