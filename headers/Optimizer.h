1f0e985ea4ba054d1b177c771211c48938a072a6
Mann Makwana <aiwithmann@gmail.com>
2025-08-24 22:03:42 +0530

    Update before the reboot

--- headers/Optimizer.h
+++ headers/Optimizer.h
@@ -3,8 +3,20 @@
 #include<Eigen/Dense>
 #include<random>
 #include "Dataset.h"
+#include "Model.h"
+#include<unordered_map>
+#include "Types.h"
+#include<memory>
 
-typedef std::vector<std::pair<Eigen::MatrixXf*, Eigen::MatrixXf*>> Trainables;
+class Model;
+
+struct ParamState{
+    Eigen::MatrixXf v;
+    Eigen::MatrixXf m;
+    Eigen::MatrixXf u;
+    Eigen::MatrixXf G;
+    int t;
+};
 typedef std::unordered_map<Eigen::MatrixXf*, ParamState> State;
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -14,16 +26,7 @@ enum class Optimizers{
     ADAM,
     RMSPROP,
     MOMENTUM,
-    NAG,
     ADAGRAD
-};
-
-struct ParamState{
-    Eigen::MatrixXf v;
-    Eigen::MatrixXf m;
-    Eigen::MatrixXf u;
-    Eigen::MatrixXf G;
-    int t;
 };
 
 class Optimizer{
@@ -31,16 +34,17 @@ friend class Model;
 protected:
     State state;
 public:
-    virtual void update() = 0;
-    virtual void init_state(Model& model) = 0;
+    virtual void updateParams() = 0;
+    virtual void init_state() = 0;
 };
 
 class SGD: public Optimizer{
     float lr;
     Trainables trainables;
 public:
-    SGD(float lr, Trainables& traiables);
-    void update();
+    SGD();
+    void initialize(float lr, Trainables& trainables);
+    void updateParams() override;
 };
 
 class Momentum: public Optimizer {
@@ -48,29 +52,20 @@ class Momentum: public Optimizer {
     float beta;
     Trainables& trainables;
 public:
-    Momentum(float lr, float beta, Trainables& trainables);
-    void init_state();
-    void update();
-};
-
-class NAG: public Optimizer {
-    float lr;
-    float beta;
-    Trainables& trainables;
-    Model& model;
-public:
-    NAG(float lr, float beta, Trainables& trainables, Model& model);
-    void init_state();
-    void update(Batch& batch);
+    Momentum();
+    void initialize(float lr, float beta, Trainables& trainables);
+    void init_state() override;
+    void updateParams() override;
 };
 
 class ADAGRAD: public Optimizer{
     float lr;
     Trainables& trainables;
 public:
-    ADAGRAD(float lr, Trainables& trainables);
-    void init_state();
-    void update();
+    ADAGRAD();
+    void initialize(float lr, Trainables& trainables);
+    void init_state() override;
+    void updateParams() override;
 };
 
 class RMSPROP: public Optimizer{
@@ -78,9 +73,10 @@ class RMSPROP: public Optimizer{
     float dRate;
     Trainables& trainables;
 public:
-    RMSPROP(float lr, float dRate, Trainables& Trainables);
-    void init_state();
-    void update();
+    RMSPROP();
+    void initialize(float lr, float dRate, Trainables& trainables);
+    void init_state() override;
+    void updateParams() override;
 };
 
 class ADAM: public Optimizer{
@@ -89,7 +85,8 @@ class ADAM: public Optimizer{
     float Beta2;
     Trainables& trainables;
 public:
-    ADAM(float lr, float Beta1, float Beta2, Trainables& trainables);
-    void init_state();
-    void update();
+    ADAM();
+    void initialize(float lr, float Beta1, float Beta2, Trainables& trainables);
+    void init_state() override;
+    void updateParams() override;
 };
