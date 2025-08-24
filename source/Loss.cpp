80f7d5dc30464f53606abba07e598628184c2a00
Mann Makwana <aiwithmann@gmail.com>
2025-08-24 22:04:59 +0530

    Update before the reboot

--- source/Loss.cpp
+++ source/Loss.cpp
@@ -1,32 +1,25 @@
-#include <iostream>
 #include <Eigen/Dense>
-
-enum class Loss{
-    MSE,
-    MAE,
-    BINARY_CROSS_ENTROPY,
-    CATEGORICAL_CROSS_ENTROPY
-};
+#include "/home/aiwithmann/Desktop/Forever-Beta/libnn-cpp/headers/Loss.h"
 
 bool shapeMismatch(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
     return True.rows() != Predicted.rows() || True.cols() != Predicted.cols();
 }
 
-inline float MSE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
+float MSE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
     return (True - Predicted).array().square().mean();
 }
 
-inline float MAE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
+float MAE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
     return (True - Predicted).array().abs().mean();
 }
 
-inline float BinaryCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
+float BinaryCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
@@ -40,7 +33,7 @@ inline float BinaryCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predic
     return -(term1 + term2).mean();
 }
 
-inline float MultiCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
+float MultiCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
@@ -53,7 +46,7 @@ inline float MultiCE(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predict
     return -log_probs.rowwise().sum().mean(); 
 }
 
-inline Eigen::MatrixXf MSEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
+Eigen::MatrixXf MSEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
@@ -63,7 +56,7 @@ inline Eigen::MatrixXf MSEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixX
     return grad.matrix();
 }
 
-inline Eigen::MatrixXf MAEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
+Eigen::MatrixXf MAEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
@@ -73,16 +66,16 @@ inline Eigen::MatrixXf MAEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixX
     return grad.matrix();
 }
 
-inline Eigen::MatrixXf BinaryCEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
+Eigen::MatrixXf BinaryCEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
-    Eigen::ArrayXf grad = (Predicted.array() - True.array())/Predicted.array()* (1- Predicted.array());
+    Eigen::ArrayXf grad = (Predicted.array() - True.array()) / (Predicted.array() * (1 - Predicted.array()));
     grad /= bSize;
     return grad.matrix();
 }
 
-inline Eigen::MatrixXf MultiCEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
+Eigen::MatrixXf MultiCEgrad(const Eigen::MatrixXf& True, const Eigen::MatrixXf& Predicted, int bSize){
     if (shapeMismatch(True, Predicted)){
         throw std::logic_error("shape mismatch");
     }
